a9bd14fb047e95af711a4b8968f88c3b544be824
Valdemar1988 <qwertysenator@gmail.com>
2023-05-23 21:45:15 +0300

    release v1

---
+++ task.py
@@ -0,0 +1,28 @@
+number_1 = "1658251651"
+number_2 = "1054141"
+
+def summ(number1:str, number2:str) -> str:
+    if len(number1) > len(number2):
+        i = len(number1) - 1
+        number2 = "0" * (len(number1) - len(number2)) + number2
+    else:
+        i = len(number2) - 1
+        number1 = "0" * (len(number2) - len(number1)) + number1
+    memory = 0
+    result = ""
+    num = ""
+    while i >= 0:              
+        if int(number1[i]) + int(number2[i]) > 9:
+            num = int(number1[i]) + int(number2[i]) + memory
+            result = str(num - 10) + result
+            memory += 1
+        else:
+            num = int(number1[i]) + int(number2[i]) + memory
+            result = str(num) + result
+            memory = 0
+        i -= 1
+    return(result)
+
+print(summ(number_1, number_2))
+print(int(number_1) + int(number_2))
+
